c0f72174a316fe1cea3c130091f01b60e03aefae
Gustavo <gugaswhom@gmail.com>
2016-02-17 16:56:38 +0000

    Added the skeleton for the Thread handling file.

---
+++ src/threads.c
@@ -0,0 +1,15 @@
+/*
+ * Threads Source file to aid with thread execution and the scheduling of any
+ * synchronization.
+ */
+
+typedef struct
+{
+	char *name;
+
+} Thread;
+
+Thread createThread(char *name)
+{
+
+}
