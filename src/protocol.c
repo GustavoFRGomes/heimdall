fc6f6703bbaafbbd19c5dce4b218a45c48d31075
Gustavo <gugaswhom@gmail.com>
2016-02-25 17:42:48 +0000

    Added more methods to the protocol source.
    Added a method to iterate through an array of protocols to check each
    for them once a packet is received. Also made a method to generate a
    Unknown Protocol whenever the protocol doesn't correspond to one on the
    list. It is a error value if you must but it could also be a indication
    that the protocol isn't on the list of unsecure ones for example.

--- src/protocol.c
+++ src/protocol.c
@@ -15,9 +15,33 @@ typedef struct
 char checkprotocol(const char* recvd, const char *template)
 {
 	// This functin will check if the template is in the received data packet.
-	
-	if (strstr(recvd, template) == NULL)
+
+	if(strstr(recvd, template) == NULL)
 		return 1;
 
 	return 0;
+}
+
+PROTOCOL unknownProtocol()
+{
+	// Whenever there is a unknonw protocol this is it.
+	PROTOCOL *unknown = (PROTOCOL*)malloc(sizeof(PROTOCOL));
+	unknown.name = (char*)malloc(sizeof(char)*7);
+
+	unknown.name = "UNKNOWN";
+	unknown.header = NULL;
+	return unknown;
+}
+
+PROTOCOL protocolID(const char* packet, const PROTOCOL list[], int list_size)
+{
+	// check for a list of protocols.
+	int i;
+
+	for(i=0; i<list_size; i++)
+	{
+		if(checkprotocol(packet, list[i].header))
+			return list[i];
+	}
+	return unknownProtocol();
 }
