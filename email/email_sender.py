bc9dab380f684ad3a6f0e118443632f340c23e2a
Gustavo <gugaswhom@gmail.com>
2016-05-13 15:06:50 +0000

    Added the email script and the template
    Added the entire script with parser of json for the template provided by
    the JSON file. Already tested and works especially for gmail with the
    setting for less secure applications to be enabled or it won't let the
    user authenticate against the server.

---
+++ email/email_sender.py
@@ -0,0 +1,42 @@
+import smtplib
+import json
+
+email_servers = {
+                 'gmail': 'smtp.gmail.com',
+                 'outlook': 'smtp-mail.outlook.com',
+                 'yahoo': 'smtp.mail.yahoo.com',
+                 'at&t': 'smtp.mail.att.net', # port 465
+                 'verizon': 'smtp.verizon.net', #port 465
+                 'comcast': 'smtp.comcast.net'
+                }
+
+def send_email(username, password, provider='gmail', template='email.json'):
+    port = 587 # to use the TLS conection
+    if provider in ['at&t', 'verizon']:
+        port = 465
+
+    smtpObj = smtplib.SMTP(email_servers[provider]+':587')
+    smtpObj.ehlo()
+    smtpObj.starttls()
+    smtpObj.login(username, password)
+
+    receiver, message = parseJson(template)
+
+    smtpObj.sendmail(username, receiver, message)
+    smtpObj.quit()
+
+def parseJson(template):
+    text = open(template)
+    temp = json.loads(text.read())['email']
+
+    receiver = temp['email_to']
+    subject = 'Subject: ' + temp['subject'] + '\n'
+    message = temp['greeting'] + ' '
+    message += temp['users_name'] + ',\n\n'
+    message += temp['reason'] + '\n\n'
+    message += temp['goodbye'] + '\n'
+    message += temp['sign']
+
+    subject += '\n' + message
+    return receiver, subject
+
